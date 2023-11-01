784c740d2b02f198a3fdd806b278229b8a43746a
Thund3rborn <kacper.feister@outlook.com>
2023-11-01 13:25:49 +0000

    Startup project for the game, added assets

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -5,25 +5,8 @@
 using namespace sf;
 using namespace std;
 
+// This is where our game starts from
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+	return 0;
 }
