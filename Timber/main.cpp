60197e9dea77fbfd3a5515b6f7597cd6f203d274
Thund3rborn <kacper.feister@outlook.com>
2023-11-01 13:57:47 +0000

    Project properties and SFML fix. Moved the assets to other folder.

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -5,8 +5,25 @@
 using namespace sf;
 using namespace std;
 
-// This is where our game starts from
 int main()
 {
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
