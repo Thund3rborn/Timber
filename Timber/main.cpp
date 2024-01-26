2a2092cd40f793721330000d3590058ae3975ad6
Thund3rborn <kacper.feister@outlook.com>
2024-01-26 12:15:06 +0000

    Game is fully functional now. Added: sounds, flying log animations, Player control handling scripts

--- Timber/main.cpp
+++ Timber/main.cpp
@@ -1,6 +1,7 @@
 #pragma once
 //page 159
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <assert.h>
 #include <sstream>
 
@@ -138,11 +139,11 @@ int main()
 		branches[i].setOrigin(220, 20);
 	}
 
-	updateBranches(1);
+	/*updateBranches(1);
 	updateBranches(2);
 	updateBranches(3);
 	updateBranches(4);
-	updateBranches(5);
+	updateBranches(5);*/
 
 	// Prepare the player
 	Texture texturePlayer;
@@ -169,7 +170,10 @@ int main()
 	const float AXE_POSITION_RIGHT = 1075;
 	// Prepare the flying log
 	Texture textureLog;
-	textureLog.loadFromFile("data/log.png");
+	if (!textureLog.loadFromFile("data/log.png"))
+	{
+		assert(false);
+	}
 	Sprite spriteLog;
 	spriteLog.setTexture(textureLog);
 	spriteLog.setPosition(810, 720);
@@ -177,6 +181,40 @@ int main()
 	bool logActive = false;
 	float logSpeedX = 1000;
 	float logSpeedY = -1500;
+	// Control the player input
+	bool acceptInput = false;
+
+	// Prepare the sounds
+	// The player chopping sound
+	SoundBuffer chopBuffer;
+	if (!chopBuffer.loadFromFile("data/woodchop.wav")) 
+	{
+		// Handle error, print message, or assert
+		std::printf("Error: Failed to load woodchop.wav");
+		assert(false);
+	}
+	Sound chop;
+	chop.setBuffer(chopBuffer);
+	// The player has met his end under a branch
+	SoundBuffer deathBuffer;
+	if (!deathBuffer.loadFromFile("data/plank.wav"))
+	{
+		// Handle error, print message, or assert
+		std::printf("Error: Failed to load plank.wav");
+		assert(false);
+	}
+	Sound death;
+	death.setBuffer(deathBuffer);
+	// Out of time
+	SoundBuffer ootBuffer;
+	if (!ootBuffer.loadFromFile("data/out_of_time.wav"))
+	{
+		// Handle error, print message, or assert
+		std::printf("Error: Failed to load out_of_time.wav");
+		assert(false);
+	}
+	Sound outOfTime;
+	outOfTime.setBuffer(ootBuffer);
 
 	while (window.isOpen())
 	{
@@ -185,6 +223,20 @@ int main()
 		Handle the players input
 		****************************************
 		*/
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyReleased && !paused)
+			{
+				// Listen for key presses again
+				acceptInput = true;
+				// hide the axe
+				spriteAxe.setPosition(2000,
+					spriteAxe.getPosition().y);
+			}
+		}
+
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
@@ -197,7 +249,73 @@ int main()
 			// Reset the time and the score
 			score = 0;
 			timeRemaining = 6;
+
+			// Make all the branches disappear -
+			// starting in the second position
+			for (int i = 1; i < NUM_BRANCHES; i++)
+			{
+				branchPositions[i] = side::NONE;
+			}
+			// Make sure the gravestone is hidden
+			spriteRIP.setPosition(675, 2000);
+			// Move the player into position
+			spritePlayer.setPosition(580, 720);
+			acceptInput = true;
 		}
+
+		// Wrap the player controls to
+		// Make sure we are accepting input
+		if (acceptInput)
+		{
+			// More code here next...
+
+			// First handle pressing the right cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Right))
+			{
+				// Make sure the player is on the right
+				playerSide = side::RIGHT;
+				score++;
+				// Add to the amount of time remaining
+				timeRemaining += (2 / score) + .15;
+				spriteAxe.setPosition(AXE_POSITION_RIGHT,
+					spriteAxe.getPosition().y);
+				spritePlayer.setPosition(1200, 720);
+				// Update the branches
+				updateBranches(score);
+
+				// Set the log flying to the left
+				spriteLog.setPosition(810, 720);
+				logSpeedX = -5000;
+				logActive = true;
+				acceptInput = false;
+
+				// Play a chop sound
+				chop.play();
+			}
+			// Handle the left cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Left))
+			{
+				// Make sure the player is on the left
+				playerSide = side::LEFT;
+				score++;
+				// Add to the amount of time remaining
+				timeRemaining += (2 / score) + .15;
+				spriteAxe.setPosition(AXE_POSITION_LEFT,
+					spriteAxe.getPosition().y);
+				spritePlayer.setPosition(580, 720);
+				// update the branches
+				updateBranches(score);
+				// set the log flying
+				spriteLog.setPosition(810, 720);
+				logSpeedX = 5000;
+				logActive = true;
+				acceptInput = false;
+
+				// Play a chop sound
+				chop.play();
+			}
+		}
+
 		/*
 		****************************************
 		Update the scene
@@ -228,6 +346,9 @@ int main()
 					textRect.top +
 					textRect.height / 2.0f);
 				messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+				// Play the out of time sound
+				outOfTime.play();
 			}
 
 			// Setup the bee
@@ -362,6 +483,50 @@ int main()
 					// Hide the branch
 					branches[i].setPosition(3000, height);
 				}
+			}
+
+			// Handle a flying log
+			if (logActive)
+			{
+				spriteLog.setPosition(
+					spriteLog.getPosition().x +
+					(logSpeedX * dt.asSeconds()),
+
+					spriteLog.getPosition().y +
+					(logSpeedY * dt.asSeconds()));
+				// Has the log reached the right hand edge?
+				if (spriteLog.getPosition().x < -100 ||
+					spriteLog.getPosition().x > 2000)
+				{
+					// Set it up ready to be a whole new log next frame
+					logActive = false;
+					spriteLog.setPosition(810, 720);
+				}
+			}
+
+			// has the player been squished by a branch?
+			if (branchPositions[5] == playerSide)
+			{
+				// death
+				paused = true;
+				acceptInput = false;
+
+				// Draw the gravestone
+				spriteRIP.setPosition(525, 760);
+				// hide the player
+				spritePlayer.setPosition(2000, 660);
+				// Change the text of the message
+				messageText.setString("SQUISHED!!");
+				// Center it on the screen
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left +
+					textRect.width / 2.0f,
+					textRect.top + textRect.height / 2.0f);
+				messageText.setPosition(1920 / 2.0f,
+					1080 / 2.0f);
+
+				// Play the death sound
+				death.play();
 			}
 
 		} // End if(!paused)
